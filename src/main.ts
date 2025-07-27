d34dd1cc73969f7f4290bc0045c96f45dbed767f
Amit <amit0805kumar@gmail.com>
2025-07-27 18:12:24 +0530

    refactor: enhance CORS configuration to allow specific origins

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const allowedOrigins = process.env.CORS_ORIGIN?.split(',') || [];
   const app = await NestFactory.create(AppModule);
-    app.enableCors({
-    origin: process.env.CORS_ORIGIN, // Allow all origins by default, can be overridden by environment variable
+  app.enableCors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin like mobile apps or curl
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow credentials if needed
   });
